9c25b322a778e4d360b064d2b14eb5ea82e34007
andrewjpage <andrewjpage@gmail.com>
2015-03-23 16:23:26 +0000

    clusters spreadsheet reader

---
+++ lib/Bio/TypingGenerator/ClusterSpreadsheetReader.pm
@@ -0,0 +1,71 @@
+package Bio::TypingGenerator::ClusterSpreadsheetReader;
+
+# ABSTRACT: Find defining snps
+
+=head1 SYNOPSIS
+
+Find defining snps
+
+=cut
+
+use Moose;
+use Text::CSV;
+
+has 'filename'                => ( is => 'rw', isa => 'Str', required => 1 );
+has 'clusters_column'         => ( is => 'rw', isa => 'Int', default => 2 );
+
+has 'clusters_to_samples'    => ( is => 'ro', isa => 'HashRef',  lazy => 1, builder => '_build_clusters_to_samples' );
+has 'samples_to_clusters'    => ( is => 'rw', isa => 'HashRef', default => sub{{}} );
+has '_csv_parser'             => ( is => 'ro', isa  => 'Text::CSV',lazy     => 1, builder => '_build__csv_parser' );
+has '_input_spreadsheet_fh'   => ( is => 'ro', lazy => 1,          builder  => '_build__input_spreadsheet_fh' );
+
+
+sub BUILD {
+    my ($self) = @_;
+    $self->clusters_to_samples;
+}
+
+
+sub _build_clusters_to_samples
+{
+	my($self) = @_;
+	my %clusters_to_samples;
+	my %samples_to_clusters;
+	my $header = $self->_csv_parser->getline( $self->_input_spreadsheet_fh );
+	
+    while ( my $row = $self->_csv_parser->getline( $self->_input_spreadsheet_fh ) ) 
+    {
+		my $sample_name = $row->[0];
+		my $cluster = $row->[$self->clusters_column];
+		
+		$samples_to_clusters{$sample_name} = $cluster ;
+
+		if(!defined($clusters_to_samples{$cluster}))
+		{
+			$clusters_to_samples{$cluster} = [];
+		}
+
+	    push($clusters_to_samples{$cluster}, $sample_name);
+
+	}
+	$self->samples_to_clusters(\%samples_to_clusters);
+	return \%clusters_to_samples;
+}
+
+
+sub _build__input_spreadsheet_fh {
+    my ($self) = @_;
+    open( my $fh, $self->filename );
+    return $fh;
+}
+
+sub _build__csv_parser
+{
+  my ($self) = @_;
+  return Text::CSV->new( { binary => 1, always_quote => 1} );
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/Bio/TypingGenerator/ClusterSpreadsheetReader.t
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+use File::Slurp::Tiny qw(read_file write_file);
+
+BEGIN { unshift( @INC, './lib' ) }
+
+BEGIN {
+    use Test::Most;
+    use_ok('Bio::TypingGenerator::ClusterSpreadsheetReader');
+}
+
+my $obj;
+ok($obj = Bio::TypingGenerator::ClusterSpreadsheetReader->new(filename => 't/data/clusters_spreadsheet.csv') , 'initialise clusters spreadsheet');
+
+is_deeply($obj->samples_to_clusters, {
+          'seq6' => '3',
+          'seq3' => '2',
+          'seq7' => '3',
+          'seq9' => '4',
+          'seq10' => '4',
+          'seq2' => '1',
+          'seq8' => '3',
+          'seq1' => '1',
+          'seq4' => '2',
+          'seq5' => '2'
+        }, 'samples to clusters');
+
+is_deeply($obj->clusters_to_samples, {
+          '4' => [
+                   'seq9',
+                   'seq10'
+                 ],
+          '1' => [
+                   'seq1',
+                   'seq2'
+                 ],
+          '3' => [
+                   'seq6',
+                   'seq7',
+                   'seq8'
+                 ],
+          '2' => [
+                   'seq3',
+                   'seq4',
+                   'seq5'
+                 ]
+        }, 'clusters to samples	');
+
+
+done_testing();
