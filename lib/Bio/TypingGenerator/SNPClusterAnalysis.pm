c4db428b12bb0e9287b155b97c2d4e1c161aee4e
andrewjpage <andrewjpage@gmail.com>
2015-03-25 15:54:32 +0000

    allow for the sample name column to be changed

--- lib/Bio/TypingGenerator/SNPClusterAnalysis.pm
+++ lib/Bio/TypingGenerator/SNPClusterAnalysis.pm
@@ -15,6 +15,7 @@ use Bio::TypingGenerator::SampleSNPsGenerator;
 has 'multifasta'           => ( is => 'rw', isa => 'Str', required => 1 );
 has 'clusters_spreadsheet' => ( is => 'rw', isa => 'Str', required => 1 );
 has 'clusters_column'         => ( is => 'rw', isa => 'Int', default => 2 );
+has 'sample_name_column'      => ( is => 'rw', isa => 'Int', default => 1 );
 
 has '_cluster_spreadsheet_reader' => ( is => 'rw', isa => 'Bio::TypingGenerator::ClusterSpreadsheetReader', lazy => 1, builder => '_build__cluster_spreadsheet_reader' );
 has '_sample_snps_generator'      => ( is => 'rw', isa => 'Bio::TypingGenerator::SampleSNPsGenerator', lazy => 1, builder => '_build__sample_snps_generator' );
@@ -22,7 +23,7 @@ has '_sample_snps_generator'      => ( is => 'rw', isa => 'Bio::TypingGenerator:
 sub _build__cluster_spreadsheet_reader
 {
 	my($self) = @_;
-	return Bio::TypingGenerator::ClusterSpreadsheetReader->new(filename => $self->clusters_spreadsheet, clusters_column => $self->clusters_column);
+	return Bio::TypingGenerator::ClusterSpreadsheetReader->new(filename => $self->clusters_spreadsheet, clusters_column => $self->clusters_column,sample_name_column => $self->sample_name_column);
 }
 
 sub _build__sample_snps_generator
